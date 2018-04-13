fb122ddb8e7070391da63b70e3d440480c88aff7
Vicente Cruz <vicentesc@daer.rs.gov.br>
2018-04-13 16:51:17 -0300

    Aula 2: Cross-site Request Forgery token para evitar Spoofing de formulario

---
+++ aula2_spoofing_form.php
@@ -0,0 +1,9 @@
+<form method="POST" action="http://benim.daer.rs.gov.br/cursophp/cursophp_benin/modulo_seguranca/aula02_csrf_token.php">
+    E-mail:<br/>
+    <input type="email" name="email" /><br/><br/>
+    
+    Senha:<br/>
+    <input type="password" name="senha" /><br/><br/>
+    
+    <input type="submit" value="Enviar" />
+</form>
