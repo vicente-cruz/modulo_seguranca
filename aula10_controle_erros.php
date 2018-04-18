e72b339ce7a13ea7517cf873ca52c2758b5633aa
Vicente Cruz <vicentesc@daer.rs.gov.br>
2018-04-18 10:24:36 -0300

    Aula10: Configurando mensagens de erro no php.ini

---
+++ aula10_controle_erros.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 1) Abrir o php.ini
+ * 2) Buscar por 'error_reporting' e setar como E_ALL
+ *      ...
+ *      error_reporting = E_ALL
+ * 3) Se estiver em desenvolvimento, definir 'display_errors' como 'On'
+ *      ...
+ *      display_errors = On
+ *      Obs: pode-se definir o valor desse parâmetro direto no script. Ex:
+ *      <?php ini_set('display_errors','Off');
+ * 4) Tanto em desenv. quanto em prod., definir 'log_errors' para 'On'.
+ *      ...
+ *      log_errors = On
+ * 5) Definir o caminho de 'error_log' FORA DA PASTA PUBLIC! Ex:
+ *      ...
+ *      error_log = "C:\xampp\php\logs\error.log"
+ * 6) 
+ */
+?>
